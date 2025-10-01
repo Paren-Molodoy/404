1e1ab9d55c0baa227d12eb0b64a3dc65c2ad5256
Pavel Firsov <paulitokyn@gmail.com>
2025-10-01 22:17:19 +0500

    fix(404): Внес правки из YouGile

--- js/cookies.js
+++ js/cookies.js
@@ -1,8 +1,10 @@
 let cookies = document.querySelector ('.cookies');
 console.log(cookies);
-let cookies_button = document.querySelector ('.cookies_button');
-console.log(cookies_button);
-cookies_button.addEventListener('click', function() {
-    console.log('Клик!');
-    cookies.classList.add('cookies--hidden');
-});
+if (cookies) {
+    let cookiesButton = document.querySelector ('.button--secondary');
+    console.log(cookiesButton);
+    cookiesButton.addEventListener('click', function() {
+        console.log('Клик!');
+        cookies.classList.add('cookies--hidden');
+    });
+}
